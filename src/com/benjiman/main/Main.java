98414fe3eb39fae64741e6dfed0db9aa85f16b41
benjimansutton <benjimansutton@gmail.com>
2022-07-27 09:28:01 +0100

    Update to the While block and link to the client

--- src/com/benjiman/main/Main.java
+++ src/com/benjiman/main/Main.java
@@ -1,21 +1,41 @@
 package com.benjiman.main;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Main {
 
 
 
-    public static void main(String[] args) {
+    public static <I0Exception extends Throwable> void main(String[] args) {
 
         // try block for the serverSocket
 
         try(ServerSocket serverSocket = new ServerSocket(5000)) {
             Socket socket = serverSocket.accept();
-            System.out.println("Client");
+            System.out.println("Client Connected");
+            BufferedReader input = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()));
 
-        } catch(I0Exception e) {
-            e.printStackTrace();
+            // Create an Infinite loop for reading the data from the client
+
+            while(true) {
+                String echoString = input.readLine();
+                if(echoString.equals("exit")) {
+                    break;  // Breaks out of the infinite loop
+                }
+                output.println("Echo from server: " + echoString);
+            }
+
+            // Catch block to get the exceptions
+
+        } catch(IOException e) {
+            System.out.println("Server exception " + e.getMessage());
+
         }
     }
 }
